bef84fa33780e8154d3199d78d707c173aaefe1b
Battlekeeper <conwaywest@tech-center.com>
2023-06-27 14:25:29 -0400

    Move Inline styles to main.css

--- src/App.vue
+++ src/App.vue
@@ -36,26 +36,6 @@ function makeid(length) {
 }
 
 </script>
-<style scoped>
-.modal {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 100;
-}
-
-.modal-content {
-  background-color: white;
-  padding: 20px;
-  border-radius: 5px;
-}
-</style>
 
 <template>
   <div v-if="modalShown" class="modal">
